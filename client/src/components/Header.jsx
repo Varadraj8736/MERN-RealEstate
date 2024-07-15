b7c68ca9dcf0723c73b90f53b800ed1e3fa4fdee
Varadraj8736 <130443975+Varadraj8736@users.noreply.github.com>
2024-07-16 00:47:50 +0530

    complete update user functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -22,20 +22,23 @@ export default function Header() {
         <FaSearch className='text-slate-600'/>
       </form>
       <ul className='flex gap-4'>
-        <Link to='./'>
+        <Link to='/'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>
                 Home
             </li>
         </Link>
-        <Link to='./about'>
+        <Link to='/about'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>
                 About
             </li>
         </Link>
         
-        <Link to='./Profile'>
+        <Link to='/profile'>
         {currentUser ? (
-          <img className='rounded-full h-7 w-7 object-covert' src={currentUser.avatar} alt="profile" />
+          <img 
+            className='rounded-full h-7 w-7 object-covert' 
+            src={currentUser.avatar} 
+            alt="profile" />
         ):(
           <li className=' text-slate-700 hover:underline'>Sign in</li>
         )}
